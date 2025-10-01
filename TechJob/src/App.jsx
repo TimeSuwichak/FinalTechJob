de21c67576e57083da8048b69f6e0c23a2e908f4
phuripat-csi <phuri.pat@spumail.net>
2025-10-01 12:48:33 +0700

    thomas test update

--- TechJob/src/App.jsx
+++ TechJob/src/App.jsx
@@ -1,15 +1,10 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
-import AdminSidebar from "./components/AdminSidebar";
-import "./App.css";
+
+import router from './router';
+import { RouterProvider } from "react-router-dom";
+
 export default function App() {
-  return (
-    <div className="flex min-h-screen bg-black text-white">
-      <AdminSidebar />
-      <div className="flex-1 p-8">
-        {/* Outlet คือจุดที่ route ลูกๆ จะมา render */}
-        <Outlet />
-      </div>
-    </div>
+  return(
+    <RouterProvider router={router} />
   );
 }
